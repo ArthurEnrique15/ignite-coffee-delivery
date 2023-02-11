1b9ab91e97aa7a504f50951cccface867d1aafde
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-11 18:15:06 -0300

    feat:  create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import { ShoppingCart, MapPin } from 'phosphor-react'
+import { HeaderContainer } from './styles'
+
+import logo from '../../assets/Logo.png'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="" />
+      <div>
+        <button className="locationButton">
+          <MapPin size={22} weight="fill" />
+          <span>Belo Horizonte, MG</span>
+        </button>
+        <button className="shopCartButton">
+          <ShoppingCart size={22} weight="fill" />
+        </button>
+      </div>
+    </HeaderContainer>
+  )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 6.5rem;
+
+  padding: 2rem 10rem;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    gap: 0.75rem;
+    height: 2.375rem;
+  }
+
+  button {
+    border: 0px;
+    border-radius: 6px;
+    padding: 0.5rem;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    gap: 0.25rem;
+  }
+
+  .locationButton {
+    span {
+      color: ${(props) => props.theme['purple-dark']};
+    }
+
+    color: ${(props) => props.theme.purple};
+    background-color: ${(props) => props.theme['purple-light']};
+  }
+
+  .shopCartButton {
+    color: ${(props) => props.theme['yellow-dark']};
+    background-color: ${(props) => props.theme['yellow-light']};
+  }
+`
