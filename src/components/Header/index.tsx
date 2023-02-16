3f18c712ba6d06357cecd2df09ab460a00bf3fdd
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-16 19:54:03 -0300

    feat: add navigation between pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import { HeaderContainer } from './styles'
 
 import logo from '../../assets/Logo.png'
 import { ShopCartContext } from '../../contexts/ShopCartContext'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   const { shopCart } = useContext(ShopCartContext)
@@ -12,20 +13,24 @@ export function Header() {
 
   return (
     <HeaderContainer>
-      <img src={logo} alt="" />
+      <NavLink to="/" title="Timer">
+        <img src={logo} alt="" />
+      </NavLink>
       <div>
         <button className="locationButton">
           <MapPin size={22} weight="fill" />
           <span>Belo Horizonte, MG</span>
         </button>
-        <button className="shopCartButton">
-          {itemsCount > 0 ? (
-            <button className="itemsAmount">{itemsCount}</button>
-          ) : (
-            ''
-          )}
-          <ShoppingCart size={22} weight="fill" />
-        </button>
+        <NavLink to="/checkout" title="Timer">
+          <button className="shopCartButton">
+            {itemsCount > 0 ? (
+              <button className="itemsAmount">{itemsCount}</button>
+            ) : (
+              ''
+            )}
+            <ShoppingCart size={22} weight="fill" />
+          </button>
+        </NavLink>
       </div>
     </HeaderContainer>
   )
