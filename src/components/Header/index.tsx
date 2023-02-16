2499f18cb5421aedb8c84342f3e0cac38b353edf
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-02-16 19:46:14 -0300

    feat: add button with items amount in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react'
 import { ShoppingCart, MapPin } from 'phosphor-react'
 import { HeaderContainer } from './styles'
 
 import logo from '../../assets/Logo.png'
+import { ShopCartContext } from '../../contexts/ShopCartContext'
 
 export function Header() {
+  const { shopCart } = useContext(ShopCartContext)
+
+  const itemsCount = shopCart.length
+
   return (
     <HeaderContainer>
       <img src={logo} alt="" />
@@ -13,6 +19,11 @@ export function Header() {
           <span>Belo Horizonte, MG</span>
         </button>
         <button className="shopCartButton">
+          {itemsCount > 0 ? (
+            <button className="itemsAmount">{itemsCount}</button>
+          ) : (
+            ''
+          )}
           <ShoppingCart size={22} weight="fill" />
         </button>
       </div>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -19,19 +19,16 @@ export const HeaderContainer = styled.header`
     height: 2.375rem;
   }
 
-  button {
+  .locationButton {
     border: 0px;
     border-radius: 6px;
     padding: 0.5rem;
 
     display: flex;
-    justify-content: space-between;
     align-items: center;
 
     gap: 0.25rem;
-  }
 
-  .locationButton {
     span {
       color: ${(props) => props.theme['purple-dark']};
     }
@@ -41,7 +38,40 @@ export const HeaderContainer = styled.header`
   }
 
   .shopCartButton {
+    border: 0px;
+    border-radius: 6px;
+
+    padding: 0.5rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
     color: ${(props) => props.theme['yellow-dark']};
     background-color: ${(props) => props.theme['yellow-light']};
+  }
+
+  .itemsAmount {
+    position: absolute;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 2px;
+
+    margin-top: calc(0px - 1rem);
+    margin-right: calc(0px - 2rem);
+
+    width: 1.15rem;
+    height: 1.15rem;
+
+    border-radius: 100%;
+
+    font-weight: 700;
+    font-size: 0.75rem;
+
+    color: ${(props) => props.theme.white};
+    background-color: ${({ theme }) => theme['yellow-dark']};
   }
 `
